cc2018e707dab7ff8913d045c3ec250d09509446
Arantxa Gandia <aragansan@alu.edu.gva.es>
2024-02-09 17:19:28 +0100

    contraseñas iguales al editar

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import StudentForm from '@/views/StudentForm.vue'
 import HomeView from '@/views/Home.vue'
 import LoginForm from '@/views/LoginForm.vue'
 import StudentEditForm  from '@/views/StudentEditForm.vue'
+import ListOffer from '@/views/ListOffer.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       name: 'student-mod',
       component: StudentEditForm,
       props: true
+    },
+    {
+      path: '/listOffers',
+      name: 'listOffers',
+      component: ListOffer
     }
   ]
 })
