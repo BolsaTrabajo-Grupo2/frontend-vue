39ef8f585efa5966b01b5f927bafc5b1c13dee9a
María Ruiz <mrp990305@gmail.com>
2024-02-13 16:16:03 +0100

    [ADD-MOD] OFERTAS

--- src/components/OffertCart.vue
+++ src/components/OffertCart.vue
@@ -6,7 +6,19 @@ export default {
 }
 </script>
 <template>
-    <h5>{{ offer.description }}</h5>
-    <p>"Jornada: " {{ offer.duration }}</p>
+    <div class="col-6 bg-secondary">
+        <h5>{{ offer.description }}</h5>
+        <p>"Jornada: " {{ offer.duration }}</p>
+        <button v-if="offer.inscriptionMethod == 1" class="apuntarse btn btn-success">Apuntarse</button>
+        <button class="details btn btn-info">Detalles</button>
+    </div>
 </template>
-<style></style>
+<style scoped>
+.apuntarse {
+    margin-right: 5px;
+}
+
+.btn {
+    margin-bottom: 10px;
+}
+</style>
