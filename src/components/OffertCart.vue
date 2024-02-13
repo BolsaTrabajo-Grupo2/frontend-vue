22eee607da2baa5310753aa151b5b0a9f36f2926
María Ruiz <mrp990305@gmail.com>
2024-02-13 16:26:25 +0100

    [ADD] showDetails

--- src/components/OffertCart.vue
+++ src/components/OffertCart.vue
@@ -2,6 +2,11 @@
 export default {
     props: {
         offer: Object,
+    },
+    methods:{
+        showDetails(){
+            
+        }
     }
 }
 </script>
@@ -10,7 +15,7 @@ export default {
         <h5>{{ offer.description }}</h5>
         <p>"Jornada: " {{ offer.duration }}</p>
         <button v-if="offer.inscriptionMethod == 1" class="apuntarse btn btn-success">Apuntarse</button>
-        <button class="details btn btn-info">Detalles</button>
+        <button class="details btn btn-info" @click="showDetails">Detalles</button>
     </div>
 </template>
 <style scoped>
